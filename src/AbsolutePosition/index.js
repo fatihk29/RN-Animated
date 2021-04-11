20fd6d2f57f3618adaff1a2e9a821c5700e191f2
fatihk29 <fatihkrc7@gmail.com>
2021-04-11 23:21:40 +0300

    color/bgC added

--- src/AbsolutePosition/index.js
+++ src/AbsolutePosition/index.js
@@ -5,17 +5,17 @@ import {
   View,
   Animated,
   TouchableWithoutFeedback,
+  Text,
 } from 'react-native';
 
 export default class animations extends Component {
   state = {
     animation: new Animated.Value(0),
   };
-
   startAnimation = () => {
     Animated.timing(this.state.animation, {
-      toValue: 1,
-      duration: 500,
+      toValue: 300,
+      duration: 1500,
       useNativeDriver: false,
     }).start(() => {
       Animated.timing(this.state.animation, {
@@ -27,30 +27,15 @@ export default class animations extends Component {
   };
 
   render() {
-    const boxInterpolation = this.state.animation.interpolate({
-      inputRange: [0, 1],
-      outputRange: ['rgb(255, 99, 71)', 'rgb(99, 71, 255)'],
-    });
-
-    const colorInterpolation = this.state.animation.interpolate({
-      inputRange: [0, 1],
-      outputRange: ['rgb(99, 71, 25)', 'rgb(99, 71, 255)'],
-    });
-
-    const boxAnimatedStyle = {
-      backgroundColor: boxInterpolation,
-    };
-
-    const textAnimatedStyle = {
-      color: colorInterpolation,
+    const animatedStyles = {
+      top: this.state.animation,
+      left: this.state.animation,
     };
     return (
       <View style={styles.container}>
         <TouchableWithoutFeedback onPress={this.startAnimation}>
-          <Animated.View style={[styles.box, boxAnimatedStyle]}>
-            <Animated.Text style={textAnimatedStyle}>
-              AAAAAAAAAAAAAA
-            </Animated.Text>
+          <Animated.View style={[styles.box, animatedStyles]}>
+            <Text>AA</Text>
           </Animated.View>
         </TouchableWithoutFeedback>
       </View>
@@ -66,6 +51,8 @@ const styles = StyleSheet.create({
   },
   box: {
     position: 'absolute',
+    left: 0,
+    top: 0,
     width: 150,
     height: 150,
     backgroundColor: 'tomato',
