b4302ba086c759abb3ae87d09a565a923efca273
fatihk29 <fatihkrc7@gmail.com>
2021-04-12 22:38:46 +0300

    interpolation added

---
+++ src/interpolation/index.js
@@ -0,0 +1,76 @@
+import React, {Component} from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  View,
+  Animated,
+  TouchableWithoutFeedback,
+} from 'react-native';
+
+export default class animations extends Component {
+  state = {
+    animation: new Animated.Value(0),
+  };
+
+  startAnimation = () => {
+    Animated.timing(this.state.animation, {
+      toValue: 1,
+      duration: 500,
+      useNativeDriver: false,
+    }).start(() => {
+      Animated.timing(this.state.animation, {
+        toValue: 0,
+        duration: 1500,
+        useNativeDriver: false,
+      }).start();
+    });
+  };
+
+  render() {
+    const boxInterpolation = this.state.animation.interpolate({
+      inputRange: [0, 1],
+      outputRange: ['rgb(255, 99, 71)', 'rgb(99, 71, 255)'],
+    });
+
+    const colorInterpolation = this.state.animation.interpolate({
+      inputRange: [0, 1],
+      outputRange: ['rgb(99, 71, 25)', 'rgb(99, 71, 255)'],
+    });
+
+    const boxAnimatedStyle = {
+      backgroundColor: boxInterpolation,
+    };
+
+    const textAnimatedStyle = {
+      color: colorInterpolation,
+    };
+    return (
+      <View style={styles.container}>
+        <TouchableWithoutFeedback onPress={this.startAnimation}>
+          <Animated.View style={[styles.box, boxAnimatedStyle]}>
+            <Animated.Text style={textAnimatedStyle}>
+              AAAAAAAAAAAAAA
+            </Animated.Text>
+          </Animated.View>
+        </TouchableWithoutFeedback>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  box: {
+    position: 'absolute',
+    width: 150,
+    height: 150,
+    backgroundColor: 'tomato',
+    fontSize: 12,
+  },
+});
+
+AppRegistry.registerComponent('animations', () => animations);
